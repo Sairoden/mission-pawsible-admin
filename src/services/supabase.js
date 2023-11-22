4a82776e5d6bc1f8a634910b69fead15ff38d1e9
Sairoden <odingandarosa@gmail.com>
2023-11-22 19:53:13 +0800

    Add env

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
-import { supabaseKey, supabaseAdminKey } from "../../config";
 
 export const supabaseUrl = "https://fpkraiqgftsurdrscohj.supabase.co";
-export const supabase = createClient(supabaseUrl, supabaseKey);
-export const supabaseAdmin = createClient(supabaseUrl, supabaseAdminKey);
+export const supabase = createClient(
+  supabaseUrl,
+  import.meta.env.VITE_SUPABASE_KEY
+);
+export const supabaseAdmin = createClient(
+  supabaseUrl,
+  import.meta.env.VITE_SUPABASE_ADMIN_KEY
+);
